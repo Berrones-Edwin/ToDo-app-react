d41b448c7dae0cfcc40aa0801964e63f6c0a085d
Berrones Edwin <Berrones.Edwin@hotmail.com>
2021-05-22 12:58:31 -0500

    add chakra ui

--- src/components/Active.js
+++ src/components/Active.js
@@ -7,9 +7,10 @@ const Active = () => {
 
     const listFiltered = list.filter((item) => item.completed !== true);
     return (
-        <div>
-            <h3>Active</h3> <ToDoList list={listFiltered} setList={setList} />
-        </div>
+        <>
+            <h3>Active</h3> 
+            <ToDoList list={listFiltered} setList={setList} />
+        </>
     );
 };
 
